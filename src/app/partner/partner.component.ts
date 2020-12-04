5628e959dca9c1fbb92dd02e74f33e8ef8a0fd3e
rto532666 <rto532666@gmail.com>
2020-12-04 16:38:15 +0530

    image-3

--- src/app/partner/partner.component.ts
+++ src/app/partner/partner.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -14,76 +15,52 @@ export class PartnerComponent implements OnInit {
 
   partnerLogos = [
     {
-      img: "assets/img/partner-img/partner-1.png",
-      hoverImg: "assets/img/partner-img/partner-hover1.png"
+      img: "assets/img/partner-img/Python.png",
+      hoverImg: "assets/img/partner-img/Python.png"
     },
     {
-      img: "assets/img/partner-img/partner-2.png",
-      hoverImg: "assets/img/partner-img/partner-hover2.png"
+      img: "assets/img/partner-img/Numpy.png",
+      hoverImg: "assets/img/partner-img/Numpy.png"
     },
     {
-      img: "assets/img/partner-img/partner-3.png",
-      hoverImg: "assets/img/partner-img/partner-hover3.png"
+      img: "assets/img/partner-img/keras.png",
+      hoverImg: "assets/img/partner-img/keras.png"
     },
     {
-      img: "assets/img/partner-img/partner-4.png",
-      hoverImg: "assets/img/partner-img/partner-hover4.png"
+      img: "assets/img/partner-img/TensorFlow.png",
+      hoverImg: "assets/img/partner-img/TensorFlow.png"
     },
     {
-      img: "assets/img/partner-img/partner-5.png",
-      hoverImg: "assets/img/partner-img/partner-hover5.png"
+      img: "assets/img/partner-img/Scikit.png",
+      hoverImg: "assets/img/partner-img/Scikit.png"
     },
     {
-      img: "assets/img/partner-img/partner-6.png",
-      hoverImg: "assets/img/partner-img/partner-hover6.png"
+      img: "assets/img/partner-img/Panda.png",
+      hoverImg: "assets/img/partner-img/Panda.png"
     },
     {
-      img: "assets/img/partner-img/partner-7.png",
-      hoverImg: "assets/img/partner-img/partner-hover7.png"
+      img: "assets/img/partner-img/nodejs.png",
+      hoverImg: "assets/img/partner-img/nodejs.png"
     },
     {
-      img: "assets/img/partner-img/partner-8.png",
-      hoverImg: "assets/img/partner-img/partner-hover8.png"
+      img: "assets/img/partner-img/Kubernetes.png",
+      hoverImg: "assets/img/partner-img/Kubernetes.png"
     },
     {
-      img: "assets/img/partner-img/partner-9.png",
-      hoverImg: "assets/img/partner-img/partner-hover9.png"
+      img: "assets/img/partner-img/JIRA.png",
+      hoverImg: "assets/img/partner-img/JIRA.png"
     },
     {
-      img: "assets/img/partner-img/partner-10.png",
-      hoverImg: "assets/img/partner-img/partner-hover10.png"
+      img: "assets/img/partner-img/AWS.png",
+      hoverImg: "assets/img/partner-img/AWS.png"
     },
     {
-      img: "assets/img/partner-img/partner-11.png",
-      hoverImg: "assets/img/partner-img/partner-hover11.png"
+      img: "assets/img/partner-img/GCP.png",
+      hoverImg: "assets/img/partner-img/GCP.png"
     },
     {
-      img: "assets/img/partner-img/partner-12.png",
-      hoverImg: "assets/img/partner-img/partner-hover12.png"
-    },
-    {
-      img: "assets/img/partner-img/partner-13.png",
-      hoverImg: "assets/img/partner-img/partner-hover13.png"
-    },
-    {
-      img: "assets/img/partner-img/partner-14.png",
-      hoverImg: "assets/img/partner-img/partner-hover14.png"
-    },
-    {
-      img: "assets/img/partner-img/partner-15.png",
-      hoverImg: "assets/img/partner-img/partner-hover15.png"
-    },
-    {
-      img: "assets/img/partner-img/partner-16.png",
-      hoverImg: "assets/img/partner-img/partner-hover16.png"
-    },
-    {
-      img: "assets/img/partner-img/partner-17.png",
-      hoverImg: "assets/img/partner-img/partner-hover17.png"
-    },
-    {
-      img: "assets/img/partner-img/partner-18.png",
-      hoverImg: "assets/img/partner-img/partner-hover18.png"
+      img: "assets/img/partner-img/GitHub.png",
+      hoverImg: "assets/img/partner-img/GitHub.png"
     }
   ];
 
